e29cd650d4969f707f4b42580db041fd0f7720d2
Joshua <joshuablazer72@gmail.com>
2024-06-22 05:08:26 +0800

    connect databse

--- API/index.js
+++ API/index.js
@@ -1,7 +1,10 @@
 import express from 'express'
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
 const app=express();
 
+mongoose.connect(process.env.MONGO)
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
 });

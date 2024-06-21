891055697d1d967d380e8e7eaa620ed8d0bf29eb
Joshua <joshuablazer72@gmail.com>
2024-06-21 17:44:00 +0800

    header section/ nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import Signin from "./pages/Signin"
 import Signup from './pages/Signup'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 export default function App() {
   return (<BrowserRouter>
+    <Header/> {/* add things in all sections */}
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/sign-in' element={<Signin />}/>
@@ -18,6 +20,5 @@ export default function App() {
 
     </BrowserRouter>
   )
-  
 }
 

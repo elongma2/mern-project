3168d3b7edb1afe43f6c9ef706cf504add26357b
Joshua <joshuablazer72@gmail.com>
2024-07-02 03:55:57 +0800

    Finish about page

--- clientSide/src/pages/About.jsx
+++ clientSide/src/pages/About.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 
 export default function about() {
   return (
-    <div>about</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Joshua Estate</h1>
+      <p className='mb-4 text-slate-700 '>
+      At Joshua Estate, we are passionate about finding your perfect place to call home. 
+      Whether you're looking to buy, sell, or rent, our dedicated team of professionals is here to guide you through every step of the process. 
+      With years of experience in the real estate industry, we have built a reputation for excellence and integrity.
+      </p>
+      <p className='mb-4 text-slate-700 '>
+      At Joshua Estate, we are committed to honesty, transparency, and professionalism. 
+      Whether you're a first-time homebuyer or a seasoned investor, 
+      we are dedicated to helping you make informed decisions that align with your goals.
+      </p>
+      <p className='mb-4 text-slate-700 '>
+      Our mission at Joshua Estate is simple: to match you with a property that meets your needs and exceeds your expectations. 
+      We understand that finding the right home is more than just a transaction—it's about finding a space where you can create memories and thrive.
+      </p>
+    </div>
   )
 }
